89cc1da21a39050996d8ed6f13911b7bb61af0a8
Haseen08 <mohdhaseen08@gmail.com>
2022-02-08 22:00:58 +0530

    Part 3: Failing test case

---
+++ src/test/java/ItemTest.java
@@ -0,0 +1,40 @@
+//import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ItemTest {
+    Item item;
+    @BeforeEach
+    public void setup(){
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+
+        item = new Item("sweet",344);
+        item.addToOrder("Sweet corn soup",119);
+        item.addToOrder("Vegetable lasagne", 269);
+    }
+
+    @Test
+    public void adding_item_to_order_should_increase_order_size_by_1(){
+
+        //  System.out.println(item.getOrder().size());
+        int initialOrderSize = item.getOrder().size();
+        item.addToOrder("Sizzling brownie",319);
+        assertEquals(initialOrderSize+1,item.getOrder().size());
+    }
+    @Test
+    public void removing_item_to_order_should_decrease_order_size_by_1() throws itemNotFoundException {
+        int initialOrderSize = item.getOrder().size();
+        item.removeFromOrder("Sweet corn soup");
+        assertEquals(initialOrderSize-1,item.getOrder().size());
+    }
+    @Test
+    public void get_total_amount_of_order()  {
+        assertEquals(388,item.getTotalAmount());
+
+    }
+}
