1a901a5bce50e9aa4c2ce905adc71f5c6436b2c0
Haseen08 <mohdhaseen08@gmail.com>
2022-02-08 21:48:06 +0530

    Part 2: Solution

---
+++ src/main/java/Item.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Item {
+    private String name;
+    private int price;
+    private List<Item> order = new ArrayList<Item>();
+
+    public Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+    @Override
+    public String toString(){
+        return  name + ":"
+                + price
+                + "\n"
+                ;
+    }
+
+}
